b38fee7060e3bcbe284395cb9ef6249085eef329
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-02 12:21:14 -0400

    u

--- app/Http/Livewire/Admin/ComprobantesAprov.php
+++ app/Http/Livewire/Admin/ComprobantesAprov.php
@@ -9,7 +9,8 @@ class ComprobantesAprov extends Component
 {
     public function render()
     {   $suscripcions=Suscripcion::where('estado',3)->get();
-        return view('livewire.admin.comprobantes-aprov',compact('suscripcions'));
+        $suscripcions_ok=Suscripcion::where('estado',1)->paginate(5);
+        return view('livewire.admin.comprobantes-aprov',compact('suscripcions','suscripcions_ok'));
     }
 
     public function aprobar(Suscripcion $suscripcion){
